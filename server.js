e5d7d4743a28bd006c004566d0b2a9d9f7020348
Ankush Chauhan <ac.ankushchauhan.2006@gmail.com>
2024-07-27 14:25:14 +0530

    experimenting with intent pattern

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const natural = require("natural");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,17 @@ app.use(bodyParser.json("hello world"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const tokenizer = new natural.WordTokenizer();
+
+let data = { intents: [] };
+// Load the JSON data
+fs.readFile("intents.json", "utf8", (err, jsonData) => {
+  if (err) {
+    console.error("Error reading data file:", err);
+    return;
+  }
+  data = JSON.parse(jsonData);
+  console.log(data.intents);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -21,15 +33,26 @@ app.post("/chat", (req, res) => {
   console.log(token);
 
   let response = "Sorry! I didn't get that";
-  if (token.includes("hello")) {
-    response = "Hello! How can I help you today?";
-  } else if (token.includes("bye")) {
-    response = "Goodbye! Have a nice day!";
-  } else if (token.includes("name")) {
-    response = "I'm your friendly chatbot";
-  } else if (token.includes("help")) {
-    response = "Sure, I am here to help. What do you need assistance with?";
+
+  for (let intent of data.intents) {
+    if (
+      intent.patterns.some((pattern) => token.includes(pattern.toLowerCase()))
+    ) {
+      response =
+        intent.responses[Math.floor(Math.random() * intent.responses.length)];
+      break;
+    }
   }
+
+  // if (token.includes("hello")) {
+  //   response = "Hello! How can I help you today?";
+  // } else if (token.includes("bye")) {
+  //   response = "Goodbye! Have a nice day!";
+  // } else if (token.includes("name")) {
+  //   response = "I'm your friendly chatbot";
+  // } else if (token.includes("help")) {
+  //   response = "Sure, I am here to help. What do you need assistance with?";
+  // }
 
   res.json({ response });
 });
